e7b98c3e3f00ae8ed5f260102422aecb8fb9d2cd
VladyslavDeveloper <156718419+VladyslavDeveloper@users.noreply.github.com>
2025-10-23 15:08:44 +0200

    make structure .13 test add

--- app/src/test/java/com/example/myyoutube/ExampleUnitTest.java
+++ app/src/test/java/com/example/myyoutube/ExampleUnitTest.java
@@ -4,12 +4,24 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 
+import com.youtube_v.domain.myyoutube.SkipaAdd;
+
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 public class ExampleUnitTest {
+
+    @Test
+    public void setSkipaAdd(){
+
+        SkipaAdd skipaAdd = new SkipaAdd(null, null);
+
+       // int result = skipaAdd.skipVideo();
+
+       // assertEquals(5, result);
+    }
     @Test
     public void addition_isCorrect() {
         assertEquals(4, 2 + 2);
